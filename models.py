7230a4b6eff75f1c79055065725ad0fa35967b9d
Gabriel Arjona <arjona.gabriel@gmail.com>
2024-11-12 16:39:28 -0300

    Agregar extracción de enlaces desde HTML con nuevo endpoint
    Implementado el endpoint fetch_links_page para extraer enlaces de una página web usando BeautifulSoup. Se refactorizó la funcionalidad de extracción de HTML, se agregaron nuevos modelos de solicitud y respuesta para manejar la extracción de enlaces, y se actualizaron los requisitos para incluir BeautifulSoup.

--- models.py
+++ models.py
@@ -25,3 +25,24 @@ class HTMLResponse(BaseModel):
     """
     html: str
     detail: str
+
+class FetchLinksPageRequest(BaseModel):
+    link: str  # Enlace de la noticia a consultar
+    metodo: str  # Metodo de búsqueda a utilizar (por ejemplo, 'selenium' o 'requests')
+    prefixes: list[str] = []
+
+    class Config:
+        # Configuración adicional para el modelo de datos
+        populate_by_name = True   # Permite acceder a los campos usando sus nombres originales
+        json_schema_extra = {
+            "example": {
+                "link": "www.latercera.com la-tercera-domingo noticia hegemonia-a-sangre-y-fuego-la-caida-de-los-trinitarios-la-banda-que-domino-la-toma-mas-grande-de-santiago OREVKTXEFBGHRKRFSGX36DPR5M",
+                "metodo": "selenium",
+                "prefixes": ["www"]
+            }
+        }
+
+class FetchLinksPageResponse(BaseModel):
+    links: list[str] = []
+    detail: str
+
--- routes.py
+++ routes.py
@@ -1,6 +1,9 @@
+from urllib.parse import urljoin
+from urllib.parse import urlparse, urlunsplit
+from bs4 import BeautifulSoup
 from fastapi import APIRouter, Body, HTTPException, status
 from fastapi.encoders import jsonable_encoder
-from models import LinkRequest, HTMLResponse
+from models import LinkRequest, HTMLResponse, FetchLinksPageResponse, FetchLinksPageRequest
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -58,9 +61,51 @@ def fetch_html_requests(link):
     html = response.content.decode('utf-8')  # Decodifica el contenido en texto HTML
     return html, "html extraido por metodo request"
 
+def extraer_links_page_request(url,prefixes):
+    urls = {}
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        enlaces = soup.find_all('a')
+        for enlace in enlaces:
+            url = enlace.get('href')
+            if url != None and url.startswith('http') == False:
+                url = urljoin(response.url,url)
+            elif url == None:
+                url = ""
+            enlace_limpiado = limpiar_url(url)
+            urls = agregar_url(urls,enlace_limpiado) if filtrar_link(enlace_limpiado,prefixes) == True else urls
+    except Exception as e:
+        print(f"ERROR en {enlace}: {e}")
+        pass
+    return urls
 
-@router.get("/api_fetch_html_content", response_model=HTMLResponse, response_description="Consultar el html de un link", status_code=status.HTTP_200_OK)
-def upsert_new(link_request: LinkRequest = Body(...)):
+def limpiar_url(url):
+    cleaned_url = urlunsplit((*(urlparse(url)[:3]), '', ''))
+    return cleaned_url
+
+def agregar_url(unique_links, url):
+    unique_links.setdefault(url, "")
+    return unique_links
+
+def url_comienza_con(url, lista) -> bool:
+    """
+    Verifica si la URL comienza con alguno de los strings en la lista, sin usar startswith()
+    """
+    url = url.lower()
+    for inicio in lista:
+        if url[:len(inicio)].lower() == inicio.lower():
+            return True
+    return False
+
+def filtrar_link(url,prefixes):
+    if url_comienza_con(url, prefixes) and url not in prefixes:
+        return True
+    return False
+
+
+@router.get("/fetch_html", response_model=HTMLResponse, response_description="Consultar el html de un link", status_code=status.HTTP_200_OK)
+def fetch_html(link_request: LinkRequest = Body(...)):
     """
     Endpoint para obtener el HTML de una página web.
     Argumentos:
@@ -86,6 +131,41 @@ def upsert_new(link_request: LinkRequest = Body(...)):
 
         # Retornar el contenido HTML extraído y un mensaje de éxito
         return {"html": html_content, "detail": response_detail}
+    except ValueError as e:
+        # Manejar errores y retornar un mensaje de error
+        raise HTTPException(status_code=400, detail=f"Error de datos: {str(e)}")
+
+@router.get("/fetch_links_page", response_model=FetchLinksPageResponse, response_description="Extrae los links de una pagina web", status_code=status.HTTP_200_OK)
+def fetch_links_page(fetch_links_page_request: FetchLinksPageRequest = Body(...)):
+    try:
+        urls = {}
+        # Convertir el modelo de petición a un formato JSON serializable
+        fetch_links_page_request_json = jsonable_encoder(fetch_links_page_request)
+
+        link = fetch_links_page_request_json["link"]
+        prefixes = fetch_links_page_request_json["prefixes"]
+        metodo = fetch_links_page_request_json["metodo"]
+
+        # Llamar a la función de extracción según el metodo especificado
+        if metodo == "selenium":
+            html_content, response_detail = fetch_html_selenium(link)
+        elif metodo == "requests":
+            html_content, response_detail = fetch_html_requests(link)
+        else:
+            return "", "Metodo solicitado no encontrado"
+
+        soup = BeautifulSoup(html_content, 'html.parser')
+        enlaces = soup.find_all('a')
+        for enlace in enlaces:
+            url = enlace.get('href')
+            if url is not None and url.startswith('http') == False:
+                url = urljoin(link, url)
+            elif url is None:
+                url = ""
+            enlace_limpiado = limpiar_url(url)
+            urls = agregar_url(urls, enlace_limpiado) if filtrar_link(enlace_limpiado, prefixes) == True else urls
+        # Retornar el contenido HTML extraído y un mensaje de éxito
+        return {"links": list(urls), "detail": response_detail}
     except ValueError as e:
         # Manejar errores y retornar un mensaje de error
         raise HTTPException(status_code=400, detail=f"Error de datos: {str(e)}")
